ed8c3ff049790ec5d9a2864a1c78e11168bba4ab
Pavel Iliukevich <pavel.iliukevich@ontravelsolutions.com>
2021-04-05 23:49:33 +0300

    практика и домашка Lesson 1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,25 @@
+import com.sun.codemodel.internal.JForEach;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        int a = ((int) (Math.random() * 7)) + 5;
+        System.out.println(a);
+        System.out.println("Введите " + a + "целых чисел:");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int count = 0;
+        for (int i = 0; i < args.length; i++) {
+            String inputLine = reader.readLine();
+            if (Integer.parseInt(inputLine) > 0) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
 }
---
+++ src/MemoryJConsoleExample.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class MemoryJConsoleExample {
+    public static final long SLEEP = 10L;
+
+    public static void main(String[] args) throws InterruptedException {
+//        while (true){
+//            Thread.sleep(SLEEP);
+////            System.out.println("привет");
+//        }
+//        List<Object[]> arrays = new LinkedList<>();
+//        for (; ; ) {
+//            arrays.add(new Object[1000]);
+//            Thread.sleep(SLEEP);
+//        }
+
+    }
+}
