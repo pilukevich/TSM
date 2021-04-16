c6b16acc7f05296bf457197f67778111fd09d5ac
Pavel Iliukevich <pavel.iliukevich@ontravelsolutions.com>
2021-04-16 16:20:39 +0300

    delete don't need imports

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
-import com.sun.codemodel.internal.JForEach;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
