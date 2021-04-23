b8048a37c0972817339c250fd25534cf4347d71b
Pavel Iliukevich <pavel.iliukevich@ontravelsolutions.com>
2021-04-23 23:30:05 +0300

    Лекция 9: практика

--- Lesson8/src/p7/Main.java
+++ Lesson8/src/p7/Main.java
@@ -1,11 +1,8 @@
 package p7;
 
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
         Type type = Type.INTEGER;
         System.out.println(type.parse("1"));
-        ArrayList
     }
 }
